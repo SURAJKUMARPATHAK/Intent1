2452cf0df5bc868baf4614d5ae17189a6bdc184a
SURAJKUMARPATHAK <30904558+SURAJKUMARPATHAK@users.noreply.github.com>
2018-06-12 07:26:38 +0530

    My Intent Proj1

---
+++ app/src/main/java/com/my/intent/Main2Activity.java
@@ -0,0 +1,19 @@
+package com.my.intent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        TextView tv = findViewById(R.id.textView);
+
+        String data = getIntent().getExtras().getString("Name");
+        tv.setText(data);
+    }
+}
